1b87e31f81243df02843b8be435e901512eefee5
Guido Piancastelli Ramos <152944211+guido-pr@users.noreply.github.com>
2024-12-25 21:36:55 -0300

    Update prova.vhd
    fix indentation problems

--- prova.vhd
+++ prova.vhd
@@ -9,7 +9,7 @@ generic (
     port (
         clock   : in    std_logic; 
         p, z    : in    std_logic; 
-		verde1, vermelho1, amarelo1, pdverde1, pdvermelho1, verde2, vermelho2, amarelo2, pdverde2, pdvermelho2: out   std_logic_vector(W - 1 downto 0)
+	verde1, vermelho1, amarelo1, pdverde1, pdvermelho1, verde2, vermelho2, amarelo2, pdverde2, pdvermelho2: out   std_logic_vector(W - 1 downto 0)
     );
 end prova;
 
@@ -19,7 +19,7 @@ architecture behavior of prova is
 begin
     process (clock)
     begin
-		if (clock'event and clock = '1') then
+	if (clock'event and clock = '1') then
             case y is
                 when s1 =>
                     if (p = '1' or z = '1') then
@@ -28,107 +28,107 @@ begin
                         y <= s1;
                     end if;
 				
-						when s2 =>
-                        y <= sw21;
-						when sw21 =>
-                        y <= sw22;
+			when s2 =>
+                        	y <= sw21;
+			when sw21 =>
+                        	y <= sw22;
                         when sw22 =>
-                        y <= sw23;
+                        	y <= sw23;
                         when sw23 =>
-                        y <= sw24;
+                        	y <= sw24;
                         when sw24 =>
-                        y <= s3;
+                        	y <= s3;
                         when s3 =>
-                        y <= s4;
+                        	y <= s4;
                         when s4 =>
-                        y <= sw41;
+                        	y <= sw41;
                         when sw41 =>
-                        y <= sw42;
+                        	y <= sw42;
                         when sw42 =>
-                        y <= sw43;
+                        	y <= sw43;
                         when sw43 =>
-                        y <= s5;
+                        	y <= s5;
                         when s5 =>
-                        y <= s1;
+                        	y <= s1;
                         
             end case;
-		end if;
+	end if;
     end process;
 
     process (y)
     begin
         case y is
-            when s1 =>
-					verde1 <= "1";
-					vermelho1 <= "0";
-					amarelo1 <= "0";
-					pdverde1 <= "0";
-					pdvermelho1 <= "1";
-					verde2 <= "0";
-					vermelho2 <= "1";
-					amarelo2 <= "0";
-					pdverde2 <= "1";
-					pdvermelho2 <= "0";
+        	when s1 =>
+			verde1 <= "1";
+			vermelho1 <= "0";
+			amarelo1 <= "0";
+			pdverde1 <= "0";
+			pdvermelho1 <= "1";
+			verde2 <= "0";
+			vermelho2 <= "1";
+			amarelo2 <= "0";
+			pdverde2 <= "1";
+			pdvermelho2 <= "0";
 					
-            when s2 =>
-               verde1 <= "1";
-					vermelho1 <= "0";
-					amarelo1 <= "0";
-					pdverde1 <= "0";
-					pdvermelho1 <= "1";
-					verde2 <= "0";
-					vermelho2 <= "1";
-					amarelo2 <= "0";
-					pdverde2 <= "1";
-					pdvermelho2 <= "0";
+		when s2 =>
+	        	verde1 <= "1";
+			vermelho1 <= "0";
+			amarelo1 <= "0";
+			pdverde1 <= "0";
+			pdvermelho1 <= "1";
+			verde2 <= "0";
+			vermelho2 <= "1";
+			amarelo2 <= "0";
+			pdverde2 <= "1";
+			pdvermelho2 <= "0";
 					
-            when s3 =>
-					verde1 <= "0";
-					vermelho1 <= "0";
-					amarelo1 <= "1";
-					pdverde1 <= "0";
-					pdvermelho1 <= "1";
-					verde2 <= "0";
-					vermelho2 <= "1";
-					amarelo2 <= "0";
-					pdverde2 <= "1";
-					pdvermelho2 <= "0";
+        	when s3 =>
+			verde1 <= "0";
+			vermelho1 <= "0";
+			amarelo1 <= "1";
+			pdverde1 <= "0";
+			pdvermelho1 <= "1";
+			verde2 <= "0";
+			vermelho2 <= "1";
+			amarelo2 <= "0";
+			pdverde2 <= "1";
+			pdvermelho2 <= "0";
 					
-	         when s4 =>
-					verde1 <= "0";
-					vermelho1 <= "1";
-					amarelo1 <= "0";
-					pdverde1 <= "1";
-					pdvermelho1 <= "0";
-					verde2 <= "1";
-					vermelho2 <= "0";
-					amarelo2 <= "0";
-					pdverde2 <= "0";
-					pdvermelho2 <= "1";
+		when s4 =>
+			verde1 <= "0";
+			vermelho1 <= "1";
+			amarelo1 <= "0";
+			pdverde1 <= "1";
+			pdvermelho1 <= "0";
+			verde2 <= "1";
+			vermelho2 <= "0";
+			amarelo2 <= "0";
+			pdverde2 <= "0";
+			pdvermelho2 <= "1";
 					
-				when s5 =>
-					verde1 <= "0";
-					vermelho1 <= "1";
-					amarelo1 <= "0";
-					pdverde1 <= "1";
-					pdvermelho1 <= "0";
-					verde2 <= "0";
-					vermelho2 <= "0";
-					amarelo2 <= "1";
-					pdverde2 <= "0";
-					pdvermelho2 <= "1";
+		when s5 =>
+			verde1 <= "0";
+			vermelho1 <= "1";
+			amarelo1 <= "0";
+			pdverde1 <= "1";
+			pdvermelho1 <= "0";
+			verde2 <= "0";
+			vermelho2 <= "0";
+			amarelo2 <= "1";
+			pdverde2 <= "0";
+			pdvermelho2 <= "1";
 					
             when others => 
-					 verde1 <= "0";
-					vermelho1 <= "0";
-					amarelo1 <= "0";
-					pdverde1 <= "0";
-					pdvermelho1 <= "0";
-					verde2 <= "0";
-					vermelho2 <= "0";
-					amarelo2 <= "0";
-					pdverde2 <= "0";
-					pdvermelho2 <= "0";
+			verde1 <= "0";
+			vermelho1 <= "0";
+			amarelo1 <= "0";
+			pdverde1 <= "0";
+			pdvermelho1 <= "0";
+			verde2 <= "0";
+			vermelho2 <= "0";
+			amarelo2 <= "0";
+			pdverde2 <= "0";
+			pdvermelho2 <= "0";
 					
         end case;
     end process;
